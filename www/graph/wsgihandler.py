5dba2032b90914fb2a2e6b31c4cd4ab5b3f31725
smallelephant9516 <52638056+smallelephant9516@users.noreply.github.com>
2019-11-26 19:09:52 -0500

    Add files via upload

---
+++ www/graph/wsgihandler.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0,'./python/')
+from ctfsim import server as application
---
+++ www/index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>CTF Simulation</title>
+ </head>
+ <body>
+ <?php echo '<br><br><center><h2>CTF Simulation</h2></center>'; ?> 
+ </body>
+</html>
